281d6c752e27431ec392ed8ba79d7afeed74838b
JadeWang-919 <angelawangqi@hotmail.com>
2025-01-24 17:34:09 -0500

    Finish refactoring - fetch games with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,10 @@
 import axois from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axois.create({
   baseURL: "https://api.rawg.io/api",
   params: { key: "06eaaa5a4b4b4a3aac0b74170ae64979" },
