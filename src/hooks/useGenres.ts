177eec3384cb4ca15be5807b48c68fcdc02f8cfc
JadeWang-919 <angelawangqi@hotmail.com>
2025-01-30 22:55:29 -0500

    Refactor - create lookup hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -16,6 +16,6 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 1000 * 60 * 60 * 24, // 24 hours
-    initialData: { count: genres.length, results: genres },
+    initialData: genres,
   });
 export default useGenres;
